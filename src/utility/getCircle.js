9bdf5d80e1d93d89db92ec0a59ca7fa913bdb393
radovanbotik <radovanbotik@gmail.com>
2023-01-20 11:26:22 +0100

    ''

---
+++ src/utility/getCircle.js
@@ -0,0 +1,15 @@
+//Utility Function
+let centerX = window.innerWidth / 2;
+let centerY = window.innerHeight / 2;
+
+Math.getDistance = function (x1, y1, x2, y2) {
+  var xs = x2 - x1,
+    ys = y2 - y1;
+  xs *= xs;
+  ys *= ys;
+  return Math.sqrt(xs + ys);
+};
+
+let radius = Math.getDistance(0, 0, centerX, centerY);
+let fullWidth = radius * 2;
+let percentIncrease = fullWidth / 100;
